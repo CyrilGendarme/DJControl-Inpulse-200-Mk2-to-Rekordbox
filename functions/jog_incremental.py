644303173e3fa68a20f4e3edc9d7e829ea8ce041
CyrilGendarme <cyrilgendarme@gmail.com>
2026-04-08 11:40:29 +0200

    fix some funcs parametrization

--- functions/jog_incremental.py
+++ functions/jog_incremental.py
@@ -1,6 +1,12 @@
 import mido
 
-def jog_incremental(ims, midi_out, wheel_messages_counter, TURN_CLOCK_SPEED, TURN_COUNTER_SPEED, JOG_SIDE_CODE, JOG_TOP_CODE):
+from djcontrol_specific.controller_notes import JOG_SIDE_CODE, JOG_TOP_CODE
+
+TURN_CLOCK_SPEED = 0x46
+TURN_COUNTER_SPEED = 0x3A
+
+
+def jog_incremental(ims, midi_out, wheel_messages_counter):
     if ims.control == JOG_SIDE_CODE or ims.control == JOG_TOP_CODE:
         wheel_messages_counter += 1
         if wheel_messages_counter % 4 == 0:
